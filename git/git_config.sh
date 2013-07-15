3f7507792d1e58f3bca15471c7fde8b4f696e44b
Ignacio Rossi <ignacio@brightsector.com>
2013-07-15 10:39:49 -0300

    Added git global config

---
+++ git/git_config.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Ignacio Rossi"
+git config --global user.email "rossi.ignacio@gmail.com"
+git config --global push.default current
+git config --global core.editor vim
+git config --global color.ui true
+git config --global merge.conflictstyle diff3
