d56eec26a879ba0c51fc861e36be3484e966bfdb
Ignacio Rossi <rossi.ignacio@gmail.com>
2013-07-25 22:21:10 -0300

    Using flags to avoid repeating git configuration

--- git/git_config.sh
+++ git/git_config.sh
@@ -1,6 +1,14 @@
-git config --global user.name "Ignacio Rossi"
-git config --global user.email "rossi.ignacio@gmail.com"
-git config --global push.default current
-git config --global core.editor vim
-git config --global color.ui true
-git config --global merge.conflictstyle diff3
+
+GIT_CONFIG_FLAG=$FLAGS_DIR/.git
+
+if [ -f $GIT_CONFIG_FLAG ]; then
+  echo "Git config is already processed"
+else
+  git config --global user.name "Ignacio Rossi";
+  git config --global user.email "rossi.ignacio@gmail.com";
+  git config --global push.default current;
+  git config --global core.editor vim;
+  git config --global color.ui true;
+  git config --global merge.conflictstyle diff3;
+  touch $GIT_CONFIG_FLAG
+fi
