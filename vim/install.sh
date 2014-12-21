ddf8763ae51c2b48832c47626b47043e2a51a38d
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-12-20 21:00:09 -0300

    html template

--- vim/install.sh
+++ vim/install.sh
@@ -18,7 +18,7 @@ else
   fc-cache
 fi
 
-for extension in py; do
+for extension in py html; do
   src="$CLONE_DIR/vim/files/template.$extension"
   dst="$HOME/.vim-template:*.$extension"
   if [[ ! -e "$dst" ]]; then
