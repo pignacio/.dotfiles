b1695690df20e27b789d9bdc44b8d99dfec1b572
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-01-15 12:42:29 -0300

    Nicer npm installs

--- npm/install.sh
+++ npm/install.sh
@@ -3,11 +3,20 @@ set -u
 
 . $CLONE_DIR/tools/log.sh
 
-mkdir -p ~/.local/bin
-if [ ! -e ~/.local/bin/jshint ]; then
-  log_title "Installing JSHint"
-  (cd ~/.local && npm install jshint)
-  ln -s ~/.local/node_modules/jshint/bin/jshint ~/.local/bin
-else
-  log_info "JSHint is already installed"
-fi
+npm_install_binary () {
+  local package=$1
+  local binary=${2:-$package}
+  local src=${3:-bin/$binary}
+  local local_dir="$HOME/.local"
+  local local_bin="$local_dir/bin/$binary"
+  mkdir -p $(dirname "$local_bin")
+  if [ ! -e "$local_bin" ]; then
+    [ -h "$local_bin" ] && rm "$local_bin";
+    log_title "Installing npm binary '$binary' from package '$package'";
+    ( cd $local_dir && npm install $package && ln -s "$local_dir/node_modules/$package/$src" "$local_bin" )
+  else
+    log_info "npm binary $package:$binary is already installed"
+  fi
+}
+
+npm_install_binary jshint
