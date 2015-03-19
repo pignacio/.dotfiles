8bb0d47f43c2e12e7618d8a3960c34f17471b6b5
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-03-19 13:21:20 -0300

    Using dotfiles_scripts python package instead of linking

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,22 +3,7 @@ set -u
 
 . $CLONE_DIR/tools/log.sh;
 
-DOTFILES_SCRIPTS_DIR=$CLONE_DIR/scripts/scripts
-SCRIPTS_DIR="$HOME/.dotfiles_scripts"
+log_title "Installing dotfiles_scripts"
 
-
-if [ -e $SCRIPTS_DIR ] || [ -h $SCRIPTS_DIR ]; then
-  if [ "$(readlink -f $SCRIPTS_DIR)" = "$(readlink -f $DOTFILES_SCRIPTS_DIR)" ]; then
-    log_info "$SCRIPTS_DIR is pointing to the right location"
-  else
-    log_info "$SCRIPTS_DIR is not pointing to the right location"
-    OLD_DIR="$HOME/.old_dotfiles_scripts-$RANDOM"
-    log_info "Moving $SCRIPTS_DIR to $OLD_DIR"
-    mv "$SCRIPTS_DIR" "$OLD_DIR"
-  fi
-fi
-
-if [ ! -e "$SCRIPTS_DIR" ]; then
-  log_info "Linking $SCRIPTS_DIR TO $DOTFILES_SCRIPTS_DIR"
-  ln -s "$DOTFILES_SCRIPTS_DIR" "$SCRIPTS_DIR"
-fi
+pipsi uninstall dotfiles_scripts --yes
+pipsi install --editable "${CLONE_DIR}/scripts/dotfiles_scripts"
