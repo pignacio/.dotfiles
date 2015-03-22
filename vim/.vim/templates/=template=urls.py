5ddc22feae03c5032083318e3c8f6f163705d908
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-03-22 03:27:37 -0300

    urls.py template

---
+++ vim/.vim/templates/=template=urls.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+from __future__ import absolute_import, unicode_literals, division
+
+import logging
+
+from django.conf.urls import patterns, url
+
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+urlpatterns = patterns(  # pylint: disable=invalid-name
+    'ingredient.views',
+#    url(r'route^$', 'the.view', name='the_name'),
+)
+
