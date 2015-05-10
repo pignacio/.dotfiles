777305a6c34f8eaf152ba747e220bbf0386e3950
Ignacio Rossi <rossi.ignacio@gmail.com>
2015-05-10 12:04:55 -0300

    scripts: djrunserver: --open argument

--- scripts/dotfiles_scripts/dotfiles_scripts/django_runserver.py
+++ scripts/dotfiles_scripts/dotfiles_scripts/django_runserver.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import (
-    absolute_import, unicode_literals, division, print_function)
+from __future__ import (absolute_import, unicode_literals, division,
+                        print_function)
 
+import argparse
+import collections
 import json
 import logging
 import os
@@ -10,8 +12,57 @@ import subprocess
 import sys
 import time
 
-
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+HostPort = collections.namedtuple('HostPort', ['host', 'port'])
+
+_DEFAULT_RUNSERVER_PORT = 8000
+
+
+def _parse_hostport(text):
+    if ":" in text:
+        host, port = text.split(":", 1)
+    else:
+        host, port = None, text
+
+    try:
+        port = int(port)
+    except (ValueError, TypeError):
+        raise ValueError("Port is not an int: '{}'  (from:{})".format(port,
+                                                                      text))
+    return HostPort(host, port)
+
+
+def _hostport_to_arg(hostport):
+    if hostport.host:
+        port = hostport.port or _DEFAULT_RUNSERVER_PORT
+        return "{}:{:d}".format(hostport.host, port)
+    elif hostport.port:
+        return str(hostport.port)
+    else:
+        return None
+
+
+def _get_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "hostport",
+        type=_parse_hostport,
+        default=HostPort(None, None),
+        nargs="?",
+        help="host and port, in [host:]port format. Overrides config port")
+    parser.add_argument(
+        "--open",
+        action='store_true',
+        default=False,
+        help=("Runs the server open to the world. "
+              "Overrides the host part of the [HOSTPORT] argument."))
+    parser.add_argument(
+        "runserver_args",
+        nargs="*",
+        help="Extra arguments for the manage.py runserver command.")
+    return parser
 
 
 def _get_manage_py():
@@ -32,9 +83,10 @@ def _read_config(config_file):
         return json.load(config_fp)
 
 
-
 def main():
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    options = _get_argument_parser().parse_args()
+
     manage_py = _get_manage_py()
     config_file = os.path.join(os.path.dirname(manage_py),
                                '.djrunserver.config.json')
@@ -48,9 +100,19 @@ def main():
             logger.exception('Problems reading config')
 
     runserver = ['python', manage_py, 'runserver']
-    if 'port' in config:
-        runserver.append(str(config['port']))
-    runserver.extend(sys.argv[1:])
+    hostport = options.hostport
+
+    if 'port' in config and hostport.port is None:
+        hostport = hostport._replace(port=config['port'])
+    if options.open:
+        hostport = hostport._replace(host="0.0.0.0")
+
+    logger.info("Running @ hostport: %s", hostport)
+    hostport_arg = _hostport_to_arg(hostport)
+    if hostport_arg is not None:
+        runserver.append(hostport_arg)
+
+    runserver.extend(options.runserver_args)
 
     while True:
         logger.info('Collecting static files')
@@ -58,7 +120,7 @@ def main():
         logger.info('Compressing')
         subprocess.call(['python', manage_py, 'compress'])
         try:
-            logger.info('Running server...')
+            logger.info('Running server... (command:%s)', runserver)
             subprocess.call(runserver)
         except KeyboardInterrupt:
             # Allow Ctrl-Xing the server
